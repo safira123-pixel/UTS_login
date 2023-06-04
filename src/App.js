069ace104d6fc6ef702b91aed0a0e9086896e520
safira123-pixel <safiraistifarini@gmail.com>
2023-06-04 15:07:05 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from "./components/views/Login";
 import Register from "./components/views/Register";
 import Forgot from "./components/views/Forgot";
 import Header from "./components/Header";
+import Dashboard from "./Dashboard";
 
 const Auth = () => {
   return (
@@ -14,6 +15,7 @@ const Auth = () => {
         <Route path='/login' component={Login} />
         <Route path='/register' component={Register} />
         <Route path='/forgot-password' component={Forgot} />
+        <Route path='/dashboard' component={Dashboard} />
         <Route path='/' component={Login} />
       </Switch>
     </Router>
